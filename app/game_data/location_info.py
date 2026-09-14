27a60c18f6692ba96cd0cae644774487a590aea7
Angela Hewitt <24223498@student.uwa.edu.au>
2026-09-14 16:30:18 +0800

    Added all location info (for now)

--- app/game_data/location_info.py
+++ app/game_data/location_info.py
@@ -7,31 +7,31 @@ location_info = {
         "artifact_to_steal": "Dagger of Irithmetẽ",
         "artifact_desc": "Dagger of Irithmetẽ, an ornate blade fashioned from the tooth of a prehistoric predator. It is said that it is imbued it with the fury of the ancient ice spirits. But that's just a myth",
         "jewel_to_steal": "Heart of Ice",
-        "jewel_desc": "Heart of Ice, first discovered in 1886 by Jacques Bourget one of his earliest expeditions. The jewel is now worth over 36 million dollars",
+        "jewel_desc": "Heart of Ice, a large diamond first discovered in 1886 by Jacques Bourget during one of his earliest expeditions. The jewel is now worth over 78 million dollars",
     },
     
     "Desert": {
         "opening_sentence_location": "Your mission is to take place in the middle of the scorching desert",
         "artifact_to_steal": "Scarab's Eye",
         "artifact_desc": "Scarabs Eye, the carapase of an ancient scarab, fashioned into a broach. The shell has been bejweled with thousands of precious stones to resemble a human eye. It is believed to cast judgement upon all those it's gaze falls upon",
-        "jewel_to_steal": "",
-        "jewel_desc": "",
+        "jewel_to_steal": "Sol's Rival",
+        "jewel_desc": "Sol's Rival, a near perfect black opal hewn from desert rock. The gem is so dark as to rival the light of the sun, hence it's name. It is worth over 66 million dollars",
     },
     
     "Jungle": {
         "opening_sentence_location": "Your mission is to take place deep in the heart of the jungle",
         "artifact_to_steal": "Serpent's Jaw",
         "artifact_desc": "Serpent's Jaw, an intricately carved pendant, fashioned from the skull of a snake. It supposedly brings bad luck to whomsoever speaks with a false tounge in it's presence. But that's just a myth",
-        "jewel_to_steal": "",
-        "jewel_desc": "",
+        "jewel_to_steal": "Tiger's Claw",
+        "jewel_desc": "Tiger's Claw, one of the largest rubies in the world. It has been cut to resemble to claw of a tiger and is worth over 49 million dollars",
     },
     
     "City": {
         "opening_sentence_location": "Your mission is to take place within the very heart of the city",
         "artifact_to_steal": "Rabbit's Foot",
         "artifact_desc": "Rabbit's Foot, an intricate wooden object, carved from the bough of a sacred tree, shaped to resemble the foot of a rabbit. It is believed to bring good luck to all those who come in contact with it and is the origin of the myth of the lucky rabbit's foot",
-        "jewel_to_steal": "",
-        "jewel_desc": "",
+        "jewel_to_steal": "Maiden's Tears",
+        "jewel_desc": "Maiden's Tears, a stunning sapphire that formed in the shape of teardrop. The jewel has been the cause of more than one war and is now worth over 51 million dollars.",
     },
     
     "Ocean": {
