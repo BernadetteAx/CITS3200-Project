5fc3ccc696c6e59a01826e579e5e881cbf0cff46
Nchoden <23867057@student.uwa.edu.au>
2026-09-15 13:36:58 +0800

    revert get random mission.py

--- app/game_data/get_random_mission.py
+++ app/game_data/get_random_mission.py
@@ -29,13 +29,10 @@ def get_mission():
         mission_location, selected_mission_dict["mission_name"]
     )
     
-    challenge_types = [
-        selected_mission_dict[f"challenge_{index}"]
-        for index in range(1, 7)
-    ]
-    challenge_types.extend(challenge_types[:2])
-    challenge_nums = [f"challenge_{index}" for index in range(1, 9)]
-    for challenge_num, challenge_type in zip(challenge_nums, challenge_types):
+    # Choose a challenge for each of the 6 challenges in teh mission
+    challenge_nums = ["challenge_1", "challenge_2", "challenge_3", "challenge_4", "challenge_5", "challenge_6"]
+    for challenge_num in challenge_nums:
+        challenge_type = selected_mission_dict[challenge_num]
 
         # Add rest option, that catches if there is no option found!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         challenge_options = []
